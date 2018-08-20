6f6c7b1d5e7074e909854e7a0437ae536652b540
a526547870 <liuzhihan027@163.com>
2018-08-20 19:42:25 +0800

    第三题

--- src/chapter_1_stackandqueue/Problem_03_ReverseStackUsingRecursive.java
+++ src/chapter_1_stackandqueue/Problem_03_ReverseStackUsingRecursive.java
@@ -3,7 +3,7 @@ package chapter_1_stackandqueue;
 import java.util.Stack;
 
 public class Problem_03_ReverseStackUsingRecursive {
-
+	//递归获取栈底元素,递归插入栈
 	public static void reverse(Stack<Integer> stack) {
 		if (stack.isEmpty()) {
 			return;
@@ -12,7 +12,7 @@ public class Problem_03_ReverseStackUsingRecursive {
 		reverse(stack);
 		stack.push(i);
 	}
-
+	//递归获取栈底元素
 	public static int getAndRemoveLastElement(Stack<Integer> stack) {
 		int result = stack.pop();
 		if (stack.isEmpty()) {
@@ -31,6 +31,9 @@ public class Problem_03_ReverseStackUsingRecursive {
 		test.push(3);
 		test.push(4);
 		test.push(5);
+//		while (!test.isEmpty()) {
+//			System.out.println(test.pop());
+//		}
 		reverse(test);
 		while (!test.isEmpty()) {
 			System.out.println(test.pop());
